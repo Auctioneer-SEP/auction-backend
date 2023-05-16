ec60f63a8a7037377265725ff0b356ac61827ecb
Harsha <harshavandanapu1990@gmail.com>
2023-05-16 11:00:02 +0530

    routes added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 //  const sigupModal = require("./modals/signup");
-
+const auctionRoute = require("./routes/auctionItems");
 const bcrypt = require("bcrypt")
 var collection ;
 mongoose.set('strictQuery', true);
@@ -15,6 +15,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
+app.use("/auction",auctionRoute);
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`server is up and running on ${port}`));
